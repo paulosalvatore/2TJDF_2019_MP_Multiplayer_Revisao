4c3b916f1d67a42818d5a4c1ec21ff3256f5caec
Paulo Salvatore <paulo@salvatore.tech>
2019-10-29 09:12:56 -0300

    Added cube as spawnable prefab

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -54,6 +54,11 @@ public class PlayerController : NetworkBehaviour
     private void Update()
     {
         MovePlayer();
+
+        if (hasAuthority && Input.GetKeyDown(KeyCode.Space))
+        {
+            CmdSpawnCube();
+        }
     }
 
     private void MovePlayer()
@@ -71,5 +76,31 @@ public class PlayerController : NetworkBehaviour
             // Spin with 'Rotate'
             transform.Rotate(Vector3.up * h * Time.deltaTime);
         }
+    }
+
+    public GameObject cubePrefab;
+
+    [Command]
+    private void CmdSpawnCube()
+    {
+        // Instantiate 'cubePrefab' and hold it into 'cube' variable
+        var cube = Instantiate(cubePrefab);
+
+        // Change position of created cube to be in front of player owner
+        //  transform.position: Represent player's position
+        //  transform.forward: Represent player's "front" with magnitude 1
+        //  transform.forward * 2: Change magninude of player's forward by 2
+        //      That means cube will be positioned 2 units away from the player
+        //  All this math will be applied to cube's position
+        cube.transform.position = transform.position + transform.forward * 2;
+
+        // Change rotation of created cube, to match player's rotation
+        cube.transform.rotation = transform.rotation;
+
+        // Since cube is only instantiated on server because of [Command] annotation,
+        //  we have to spawn it on entire server, so all players can see this new cube
+        // NOTE: This will only work if cube is registered as a 'SpawnablePrefab'
+        //  on NetworkManager
+        NetworkServer.Spawn(cube);
     }
 }
